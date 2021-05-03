c767740350618cb413ce62882aa7696a8e88a0e9
unknown <love90719@naver.com>
2021-05-03 23:37:55 +0900

    Set Initial directory and pages for app

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export class App extends Component<any, any>{
+  render() {
+    return (
+        <div>
+          Retro App
+        </div>
+    );
+  }
+
+}
+
+export default App;
--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../components/App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Retro App/i);
   expect(linkElement).toBeInTheDocument();
 });
